4fba0124c2ad569438c7071a5a458a9b11eec6bd
MaxJiangYx92 <785088174@qq.com>
2018-06-14 14:31:58 +0800

    Reactive Redis Test

---
+++ spring-boot-demo/src/main/java/com/test/demo/springboot/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.test.demo.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnection;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+/**
+ * @author JiangYx
+ * @Project
+ * @Title
+ * @Package
+ * @Create 2018/6/14 10:49
+ * @Update -
+ * @Description
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public ReactiveRedisConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory("172.190.15.30", 6379);
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
+        return new ReactiveRedisTemplate<>(connectionFactory, RedisSerializationContext.string());
+    }
+}

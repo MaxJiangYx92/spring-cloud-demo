a6febaa9f074b0be3f84198972e385511d11c402
MaxJiangYx92 <785088174@qq.com>
2018-06-14 14:30:56 +0800

    Reactive Redis Test

--- spring-boot-demo/src/main/java/com/test/demo/springboot/Application.java
+++ spring-boot-demo/src/main/java/com/test/demo/springboot/Application.java
@@ -2,7 +2,7 @@ package com.test.demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author JiangYx
@@ -13,7 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @Update -
  * @Description
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- spring-boot-demo/src/main/java/com/test/demo/springboot/controller/HelloController.java
+++ spring-boot-demo/src/main/java/com/test/demo/springboot/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.test.demo.springboot.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -23,19 +25,31 @@ import java.util.List;
 public class HelloController {
     private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+//    @Autowired
+//    private DiscoveryClient client;
+
     @Autowired
-    private DiscoveryClient client;
+    private ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     @RequestMapping("/hello")
-    public String index() {
-        List<ServiceInstance> instance = client.getInstances("");
+    public Mono<String> index() {
+//        List<ServiceInstance> instance = client.getInstances("");
 //        logger.info("/hello,host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 
 //        List<String> services=client.getServices();
-        for (ServiceInstance item : instance) {
-            System.out.println("/hello,host:" + item.getHost() + ", service_id:" + item.getServiceId());
-        }
-//        System.out.println(client.getServices());
-        return "hello spring boot";
+//        for (ServiceInstance item : instance) {
+//            System.out.println("/hello,host:" + item.getHost() + ", service_id:" + item.getServiceId());
+//        }
+
+        reactiveRedisTemplate.opsForSet().add("reactive_key", "hello reactive value");
+//        reactiveRedisTemplate.opsForSet().
+        return Mono.just("hello spring boot");
+    }
+
+    @RequestMapping("/nihao")
+    public Mono<Boolean> addLink() {
+        String key = "hello_reactive_set";
+        String value = "hello_reactive_value";
+        return reactiveRedisTemplate.opsForValue().set(key, value);
     }
 }

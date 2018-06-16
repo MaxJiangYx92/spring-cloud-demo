b7a0c3503a8c64452cf393c746b064699d4888d1
MaxJiangYx92 <785088174@qq.com>
2018-06-16 16:12:39 +0800

    spring demo common

---
+++ spring-demo-common/src/main/java/com/test/demo/common/pojo/UserDO.java
@@ -0,0 +1,40 @@
+package com.test.demo.common.pojo;
+
+/**
+ * @author JiangYx
+ * @Project
+ * @Title
+ * @Package
+ * @Create 2018/6/16 15:45
+ * @Update -
+ * @Description
+ */
+public class UserDO {
+    private String name;
+    private String sex;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}

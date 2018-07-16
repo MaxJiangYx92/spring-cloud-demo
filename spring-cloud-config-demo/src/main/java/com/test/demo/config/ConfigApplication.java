cd3a3e90c59d947e1c509794a7b2ccde25e3c670
MaxJiangYx92 <785088174@qq.com>
2018-07-16 21:49:46 +0800

    spring cloud config demo

---
+++ spring-cloud-config-demo/src/main/java/com/test/demo/config/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.test.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author JiangYx
+ * @Project
+ * @Title
+ * @Package
+ * @Create 2018/7/16 20:37
+ * @Update -
+ * @Description
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);
+    }
+}
